990e32130fcb60aee87c283848c0a53574888953
Machoka Joash Omao <aaronjoash44@gmail.com>
2023-01-14 18:00:40 +0300

    added some seed data

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,8 +2,8 @@ class Client < ApplicationRecord
 	has_secure_password
 
 	has_many :reviews
-	belongs_to :trainer
-	belongs_to :diet
+	# belongs_to :trainer
+	# belongs_to :diet
 	has_many :supplements
 	has_many :exercises
 end
